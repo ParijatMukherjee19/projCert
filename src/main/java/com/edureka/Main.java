7e42114cc88efae36a632e44f194ad1e4eba0378
root <root@ip-172-31-17-78.ap-south-1.compute.internal>
2020-10-27 14:52:37 +0000

    updated

--- src/main/java/com/edureka/Main.java
+++ src/main/java/com/edureka/Main.java
@@ -1,35 +1,41 @@
 package com.edureka;
 
-import org.openqa.selenium.By;
+import java.io.IOException; 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class Main {
 
     public static void main(String[] args) {
 		
-		        System.setProperty("webdriver.chrome.driver", "chromedriver");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--headless");
-				chromeOptions.addArguments("--remote-debugging-port=9222");
+		chromeOptions.addArguments("--remote-debugging-port=9222");
                 chromeOptions.addArguments("--no-sandbox");
- 
+
+             try{ 
                 WebDriver driver = new ChromeDriver(chromeOptions);
  
                 driver.get("http://localhost:8090");
  
                 Thread.sleep(1000);
- 
-                if (driver.getPageSource().contains("About Us")) {
-                        System.out.println("Pass");
-                } else {
-                        System.out.println("Fail");
+                WebElement button = driver.findElement(By.id("About"));
+                if(button.isDisplayed()){
+                System.out.println("Button about is there: " + button.isDisplayed());
+                }else{
+                System.out.println("Fail");}
+                driver.close();
+
+                }catch (Exception ex)
+                {
+                  System.out.println("Exception occured ");
                 }
-                driver.quit();
-		
-		
-		
-    }
+
+}
+
 }
